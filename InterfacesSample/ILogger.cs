45ef1966b5ef813884cf5df37b5acb927be16476
KBonnicksen <kelseybon@gmail.com>
2019-08-25 13:34:46 -0700

    Set up DI Container with SimpleInjector

--- InterfacesSample/ILogger.cs
+++ InterfacesSample/ILogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace InterfacesSample
     public interface ILogger
     {
         void WriteData(string data);
+    }
+
+    class FileLogger : ILogger
+    {
+        void ILogger.WriteData(string data)
+        {
+            string nl = Environment.NewLine;
+
+            File.AppendAllText("log.txt", 
+                DateTime.Now.ToString() + nl + data + nl + nl);
+        }
     }
 
     class WinFormsLogger : ILogger
--- InterfacesSample/Program.cs
+++ InterfacesSample/Program.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SimpleInjector;
+using SimpleInjector.Diagnostics;
+using SimpleInjector.Lifestyles;
 
 namespace InterfacesSample
 {
     static class Program
     {
+        private static Container container;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,9 +22,30 @@ namespace InterfacesSample
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            ILogger logger = new WinFormsLogger();
+            ConfigureDIContainer();
+        }
 
-            Application.Run(new frmLogger(logger));
+        /// <summary>
+        /// Configure Dependency Injection Container.
+        /// Registers all types and forms that need to be instantiated
+        /// </summary>
+        private static void ConfigureDIContainer()
+        {
+            //Instantiate DI (Dependency Injection) container
+            container = new Container();
+            container.Options.DefaultScopedLifestyle = new ThreadScopedLifestyle();
+
+
+            // Register types
+            container.Register<ILogger, ConsoleLogger>(Lifestyle.Transient);
+            container.Register<frmLogger>(Lifestyle.Transient);
+
+            Registration r = container.GetRegistration(typeof(frmLogger)).Registration;
+            r.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, 
+                "WelcomeForm is automatically disposed");
+
+            //Optionally verify the ocntainer
+            container.Verify();
         }
     }
 }
